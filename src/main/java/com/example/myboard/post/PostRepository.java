84d49048c439b336c019d5769fdb34c532c4af9e
leeminju <leemj4090@naver.com>
2023-12-22 06:30:28 +0900

    feat(scheduler) 스케줄러 기능 구현

--- src/main/java/com/example/myboard/post/PostRepository.java
+++ src/main/java/com/example/myboard/post/PostRepository.java
@@ -3,8 +3,10 @@ package com.example.myboard.post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post,Long>,PostRepositoryCustom {
+    void deleteByModifiedAtBefore(LocalDateTime before90Days);
 }
